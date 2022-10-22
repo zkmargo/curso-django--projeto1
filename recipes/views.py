eabd7f5a8d0b663e366546189134ec38e80f81d5
zkmargo <zkmargo@gmail.com>
2022-10-22 12:02:05 -0300

    Exemplo de template temp

---
+++ recipes/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'recipes/home.html', context={
+        'name': 'Luiz Otávio'
+    })
+
+
+def contato(request):
+    return render(request, 'me-apague/temp.html')
+
+def sobre(request):
+    return HttpResponse('sobre')
+
